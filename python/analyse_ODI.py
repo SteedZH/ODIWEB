3fecfdb58375a910c6c4944340af9c18d4f68dfd
Steed <steedzh@gmail.com>
2019-03-13 20:14:03 +0000

    barchart

--- python/analyse_ODI.py
+++ python/analyse_ODI.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv("../dataset.csv")  # raw data
 
 df1 = df["country_long"].value_counts()  # number of plants in countries
-
+fuel = df["fuel1"].value_counts()
 df2 = df[["country_long", "capacity_mw", "latitude", "longitude", "fuel1"]]  # choosing rows
 # df3 = df.loc[df["capacity_mw"].isnull(), :]
 df3 = df2.dropna()  # get rid of row contained null
@@ -11,4 +11,40 @@ df3 = df2.dropna()  # get rid of row contained null
 # print(len(df), len(df1), len(df2), len(df3))
 
 # df3.to_csv("refine.csv", index=False)
+
+country = pd.read_json("countries.json")
+country = country.loc[["name", "continent"]]
+
+country.replace({
+  "AF": "Africa",
+  "AN": "Antarctica",
+  "AS": "Asia",
+  "EU": "Europe",
+  "NA": "North America",
+  "OC": "Oceania",
+  "SA": "South America"
+}, inplace=True)
+
+# country_name = df1.index.values
+df1 = df1.reset_index()
+df1 = df1.rename(columns={"index": "country", "country_long": "plant_number"})
+
+country = country.stack()
+# country.to_csv("test.csv", index=True)
+country = pd.read_csv("country_continent.csv")
+
+result = pd.merge(df1, country, how='left', on="country")
+
+# df_test = result[result["continent"].isnull()]
+# result.loc["Myanmar", "continent"] = "Asia"
+# result.loc["syrian arab republic", "continent"] = "Asia"
+# result.loc["brunei Darussalam", "continent"] = "Asia"
+# result.loc["Rhodesia", "continent"] = "Africa"
+
+# result.to_csv("join.csv")
+result = pd.read_csv("join.csv")
+test = result["continent"].value_counts()
+
+print("hello")
+
 
